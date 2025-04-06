9470182841bccdd16e77d82285df47e2546417fb
Nguyen Minh Tri <nmtri099019@gmail.com>
2025-04-06 23:27:11 +0700

    video18

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ const path=require('path')
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const route = require('./routes');
 
 const app = express()
 const port = 3000
 
+
 app.use(express.static(path.join(__dirname,'public')));
 // console.l og(path.join(__dirname,'public'))
 // app.use(morgan('combined'))
+
+//tra gia tri khi submit ve bode trong cong post
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
 
 app.engine('hbs', handlebars.engine({
   extname:'.hbs'
@@ -16,13 +24,12 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
-// console.log(path.join(__dirname,'resources/views'));
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+
+//Routes init;
+route(app);
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+function route(app) {
+    // console.log(path.join(__dirname,'resources/views'));
+    
+    // app.get('/news', (req, res) => {
+    //   res.render('news')
+    // })
+    app.use('/news',newsRouter);
+    app.use('/',siteRouter);
+    // app.get('/', (req, res) => {
+    //     res.render('home')
+    // })
+    // app.get('/search', (req, res) => {
+    //     res.render('search')
+    // })
+    // app.post('/search', (req, res) => {
+    //     console.log(req.body);
+    //     res.render('search')
+    // })
+}
+module.exports =route;
